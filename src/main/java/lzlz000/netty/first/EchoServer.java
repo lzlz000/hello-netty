fc514c89474bd004eae1f1c63b11983df362274d
lz <lzlz0810@qq.com>
2019-05-31 18:41:18 +0800

    备注

--- src/main/java/lzlz000/netty/first/EchoServer.java
+++ src/main/java/lzlz000/netty/first/EchoServer.java
@@ -12,6 +12,12 @@ import java.net.InetSocketAddress;
 
 import static lzlz000.Const.DEFAULT_PORT;
 
+/**
+ * netty的helloworld程序
+ * 实现这样一个功能
+ * 客户端从控制台输入数据，发送给服务器，服务器保存当前收到的消息，并返回从这个客户端收到的所有数据
+ * 当客户端输入BYE 服务器断开连接
+ */
 public class EchoServer {
     private final int port;
 
@@ -29,7 +35,7 @@ public class EchoServer {
     private void start() throws InterruptedException {
 //        final EchoServerHandler serverHandler = new EchoServerHandler();
         /*线程组*/
-        EventLoopGroup group = new NioEventLoopGroup(1);
+        EventLoopGroup group = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();/*服务端启动必备*/
             b.group(group)
